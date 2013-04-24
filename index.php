2d6de8193c4ae037545082c6652005fca0f3b060
Dogy Vogey <vogey@dogyvogey.com>
2013-04-24 18:25:52 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+require( "core.php" ); // Get the database(MYSQL) connection
+
+require( "functions.php" ); // Get the file with the knowledge.
+
+analitics(); // Run the analitics
+get_unic(); // Return the unic views
+get_returned(); // Get the number of returned vistiters
+?>
